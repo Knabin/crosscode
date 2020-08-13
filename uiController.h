d80bcbd565b994c4bb5a7dc42e9d550c2c15f40f
LEESUNGYO <lsk2307@naver.com>
2020-08-13 17:35:44 +0900

    ui push

--- uiController.h
+++ uiController.h
@@ -1,13 +1,13 @@
 #pragma once
 #include "gameNode.h"
-
+#include "nomalUI.h"
 // ui Controller에서는 화면 최상위에 표시되어야 할 UI type의 gameObject들을 받아와 관리합니다.
 
 class uiController : public gameNode
 {
 private:
 	vector<class gameObject*> _vUiObjects;
-
+	nomalUI* _nm;
 public:
 	HRESULT init();
 	void release();
